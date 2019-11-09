0de2d8ba29b727af92fa27aac8ded1eb0ab93ef3
Matt <mtud85@gmail.com>
2019-11-09 12:03:15 +0100

    Grid Refactoring Completato

--- main_script.py
+++ main_script.py
@@ -16,69 +16,69 @@ class Calcolatrice:
         # DIGITS BUTTONS
         self.button1 = Button(self.contenitore, command= self.button1Premuto)
         self.button1.configure(text="1")
-        self.button1.pack()
+        self.button1.grid(row=0, column=0)
 
         self.button2 = Button(self.contenitore, command= self.button2Premuto)
         self.button2.configure(text="2")
-        self.button2.pack()
+        self.button2.grid(row=0, column=1)
 
         self.button3 = Button(self.contenitore, command= self.button3Premuto)
         self.button3.configure(text="3")
-        self.button3.pack()
+        self.button3.grid(row=0, column=2)
 
         self.button4 = Button(self.contenitore, command= self.button4Premuto)
         self.button4.configure(text="4")
-        self.button4.pack()
+        self.button4.grid(row=1, column=0)
 
         self.button5 = Button(self.contenitore, command= self.button5Premuto)
         self.button5.configure(text="5")
-        self.button5.pack()
+        self.button5.grid(row=1, column=1)
 
         self.button6 = Button(self.contenitore, command= self.button6Premuto)
         self.button6.configure(text="6")
-        self.button6.pack()
+        self.button6.grid(row=1, column=2)
 
         self.button7 = Button(self.contenitore, command= self.button7Premuto)
         self.button7.configure(text="7")
-        self.button7.pack()
+        self.button7.grid(row=2, column=0)
 
         self.button8 = Button(self.contenitore, command= self.button8Premuto)
         self.button8.configure(text="8")
-        self.button8.pack()
+        self.button8.grid(row=2, column=1)
 
         self.button9 = Button(self.contenitore, command= self.button9Premuto)
         self.button9.configure(text="6")
-        self.button9.pack()
+        self.button9.grid(row=2, column=2)
 
         self.button0 = Button(self.contenitore, command= self.button0Premuto)
         self.button0.configure(text="0")
-        self.button0.pack()
+        self.button0.grid(row=3, column=1)
 
         self.button_comma = Button(self.contenitore, command = self.button_comma_Premuto)
         self.button_comma.configure(text=".")
-        self.button_comma.pack()
+        self.button_comma.grid(row=3, column=2)
 
 
         # OPERATION BUTTONS
         self.button_add = Button(self.contenitore, command= self.button_add_premuto)
         self.button_add.configure(text="+")
-        self.button_add.pack()
+        self.button_add.grid(row=0, column=3)
 
         self.button_subtract = Button(self.contenitore, command= self.button_subtract_premuto)
         self.button_subtract.configure(text="-")
-        self.button_subtract.pack()
+        self.button_subtract.grid(row=1, column=3)
 
         self.button_mult = Button(self.contenitore, command= self.button_mult_premuto)
         self.button_mult.configure(text="*")
-        self.button_mult.pack()
+        self.button_mult.grid(row=2, column=3)
 
         self.button_divide = Button(self.contenitore, command= self.button_divide_premuto)
         self.button_divide.configure(text="/")
-        self.button_divide.pack()
+        self.button_divide.grid(row=3, column=3)
 
         self.button_result = Button(self.contenitore, command= self.button_result_premuto)
         self.button_result.configure(text="=")
-        self.button_result.pack()
+        self.button_result.grid(row=3, column =0)
 
 
 
@@ -87,7 +87,7 @@ class Calcolatrice:
         # UTILITY BUTTONS
         self.abort_button = Button(self.contenitore, command = self.pulsante_abort_premuto)
         self.abort_button.configure(text="Abort")
-        self.abort_button.pack()
+        self.abort_button.grid(row=4, column=3)
 
 
 
